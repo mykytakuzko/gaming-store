c90734ea4bc1fd599477f36824d9aaacb01a8694
Nikita Kuzko <niksonkuzko@icloud.com>
2024-06-14 00:39:34 +0300

    REQ-11: Add a few clients and print them (grouped by cities)

--- src/main/java/com/gamingstore/component/ApplicationRunner.java
+++ src/main/java/com/gamingstore/component/ApplicationRunner.java
@@ -1,55 +1,11 @@
 package main.java.com.gamingstore.component;
 
-import main.java.com.gamingstore.Main;
-import main.java.com.gamingstore.model.Client;
-import main.java.com.gamingstore.model.Order;
-import main.java.com.gamingstore.service.ClientService;
-import main.java.com.gamingstore.service.OrderService;
-
 public class ApplicationRunner {
-    private final ClientService clientService = new ClientService();
-    private final OrderService orderService = new OrderService();
+    private final EntityRegister register = new EntityRegister();
 
     public void run() {
         if (Authenticator.auth()) {
-            Client client = clientService.registerNewClient();
-
-            if (client != null) {
-                System.out.print("Do you want to add an order? (Yes / No): ");
-                String answer = Main.SCANNER.nextLine();
-
-                if (answer.equals("Yes")) {
-                    registerOrders(client);
-                }
-
-                System.out.println(client);
-            }
-        }
-    }
-
-    private void registerOrders(Client client) {
-        boolean continueAddOrders = true;
-
-        while (continueAddOrders) {
-            addOrder(client);
-
-            System.out.print("Do you want to add more orders for the current client? (y/n): ");
-            String answer = Main.SCANNER.nextLine();
-
-            if ("n".equals(answer)) {
-                continueAddOrders = false;
-            }
-        }
-    }
-
-    private void addOrder (Client client) {
-        System.out.println("Adding a new order.");
-
-        Order order = orderService.registerNewOrder();
-        if (order != null) {
-            client.addOrder(order);
-            order.setClient(client.getFirstName() + " " + client.getLastName());
-            System.out.println("Order has been added.");
+            register.registerClients();
         }
     }
 }
---
+++ src/main/java/com/gamingstore/component/EntityRegister.java
@@ -0,0 +1,113 @@
+package main.java.com.gamingstore.component;
+
+import main.java.com.gamingstore.Main;
+import main.java.com.gamingstore.model.Client;
+import main.java.com.gamingstore.model.Order;
+import main.java.com.gamingstore.service.ClientService;
+import main.java.com.gamingstore.service.OrderService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EntityRegister {
+    private final ClientService clientService = new ClientService();
+    private final OrderService orderService = new OrderService();
+
+    public void registerClients() {
+        List<Client> clients = new ArrayList<>();
+
+        String message = "Do you want to register more clients? (y/n): ";
+        do {
+            Client client = addClient();
+            if (client != null) {
+                clients.add(client);
+            }
+        } while (verifyRepeating(message));
+
+        Map<Client.Location, List<Client>> clientsByLocation = groupClients(clients);
+        printClients(clientsByLocation);
+    }
+
+    private void printClients(Map<Client.Location, List<Client>> clientsByLocation) {
+        for (Map.Entry<Client.Location, List<Client>> clients : clientsByLocation.entrySet()) {
+            String content = "\nLocation: " + clients.getKey()
+                    + "\nClients (" + clients.getValue().size() + "):"
+                    + "\n\t" + clients.getValue();
+
+            System.out.println(content);
+        }
+    }
+
+    private Map<Client.Location, List<Client>> groupClients(List<Client> clients) {
+        List<Client> fromKyiv = new ArrayList<>();
+        List<Client> fromLviv = new ArrayList<>();
+        List<Client> fromOdesa = new ArrayList<>();
+
+        for (Client client : clients) {
+            switch (client.getLocation()) {
+                case KYIV -> fromKyiv.add(client);
+                case LVIV -> fromLviv.add(client);
+                case ODESA -> fromOdesa.add(client);
+            }
+        }
+
+        Map<Client.Location, List<Client>> clientsByLocation = new HashMap<>();
+        clientsByLocation.put(Client.Location.KYIV, fromKyiv);
+        clientsByLocation.put(Client.Location.LVIV, fromLviv);
+        clientsByLocation.put(Client.Location.ODESA, fromOdesa);
+
+        return clientsByLocation;
+    }
+
+    private Client addClient() {
+        Client client = clientService.registerNewClient();
+
+        if (client != null) {
+            System.out.print("Do you want to add an order? (Yes / No): ");
+            String answer = Main.SCANNER.nextLine();
+
+            if (answer.equals("Yes")) {
+                registerOrders(client);
+            }
+
+            System.out.println(client);
+        }
+
+        return client;
+    }
+
+    private void registerOrders(Client client) {
+        String message = "Do you want to add more orders for the current client? (y/n): ";
+
+        do {
+            addOrder(client);
+        } while (verifyRepeating(message));
+    }
+
+    private void addOrder (Client client) {
+        System.out.println("Adding a new order.");
+
+        Order order = orderService.registerNewOrder();
+        if (order != null) {
+            client.addOrder(order);
+            order.setClient(client.getFirstName() + " " + client.getLastName());
+            System.out.println("Order has been added.");
+        }
+    }
+
+    private boolean verifyRepeating(String message) {
+        System.out.println(message);
+
+        String answer = Main.SCANNER.nextLine();
+        if ("y".equals(answer)) {
+            return true;
+        } else if("n".equals(answer)) {
+            return false;
+        } else {
+            System.out.println("Incorrect answer. Please try again.");
+            return verifyRepeating(message);
+        }
+    }
+}
